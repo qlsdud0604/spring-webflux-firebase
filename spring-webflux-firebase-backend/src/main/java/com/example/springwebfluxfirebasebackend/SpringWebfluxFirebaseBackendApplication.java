a722cb6ad32d2efaf9468c10a002ea25c94dd820
임빈영 <qlsdud960604@gmail.com>
2021-11-19 14:57:04 +0900

    프로젝트 생성 및 환경설정

---
+++ spring-webflux-firebase-backend/src/main/java/com/example/springwebfluxfirebasebackend/SpringWebfluxFirebaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebfluxfirebasebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxFirebaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxFirebaseBackendApplication.class, args);
+	}
+
+}
---
+++ spring-webflux-firebase-backend/src/test/java/com/example/springwebfluxfirebasebackend/SpringWebfluxFirebaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwebfluxfirebasebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxFirebaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
